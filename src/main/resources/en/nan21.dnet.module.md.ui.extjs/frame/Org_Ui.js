ac1e221441a8943bc3731d9c7c0828983a774bcd
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2013-10-29 07:56:09 +0200

    Address and communication linked to organization

--- src/main/resources/en/nan21.dnet.module.md.ui.extjs/frame/Org_Ui.js
+++ src/main/resources/en/nan21.dnet.module.md.ui.extjs/frame/Org_Ui.js
@@ -1,6 +1,11 @@
 Ext.define(Dnet.ns.md + "Org_Ui$Trl" , {
 	/* view */
+	addressPanel__ttl: "Address",
+	communicEditList__ttl: "Communication",
 	/* menu */
+	tlbAddressEdit__ttl: "Address",
+	tlbAddressList__ttl: "Addresses",
+	tlbCommunicEditList__ttl: "Communication",
 	tlbOrgList__ttl: "Organizations",
 	/* button */
 	
--- src/main/resources/ro/nan21.dnet.module.md.ui.extjs/frame/Org_Ui.js
+++ src/main/resources/ro/nan21.dnet.module.md.ui.extjs/frame/Org_Ui.js
@@ -1,6 +1,11 @@
 Ext.define(Dnet.ns.md + "Org_Ui$Trl" , {
 	/* view */
+	addressPanel__ttl: "Adrese",
+	communicEditList__ttl: "Comunicare",
 	/* menu */
+	tlbAddressEdit__ttl: "Adresă",
+	tlbAddressList__ttl: "Adrese",
+	tlbCommunicEditList__ttl: "Comunicare",
 	tlbOrgList__ttl: "Organizaţii",
 	/* button */
 	
