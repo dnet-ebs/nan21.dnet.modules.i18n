454926f1ced6492c5b1510cd0010d04249cf94b9
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2013-10-31 07:47:48 +0200

    Communication method translations

---
+++ src/main/resources/en/nan21.dnet.module.bd.ui.extjs/frame/CommunicationMethod_Ui.js
@@ -0,0 +1,8 @@
+Ext.define(Dnet.ns.bd + "CommunicationMethod_Ui$Trl" , {
+	/* view */
+	/* menu */
+	tlbCmncList__ttl: "Communication",
+	/* button */
+	
+	title: "Communication"
+});
---
+++ src/main/resources/ro/nan21.dnet.module.bd.ui.extjs/frame/CommunicationMethod_Ui.js
@@ -0,0 +1,8 @@
+Ext.define(Dnet.ns.bd + "CommunicationMethod_Ui$Trl" , {
+	/* view */
+	/* menu */
+	tlbCmncList__ttl: "Comunicare",
+	/* button */
+	
+	title: "Comunicare"
+});
