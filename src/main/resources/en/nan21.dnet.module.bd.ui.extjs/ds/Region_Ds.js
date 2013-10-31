74c2ef0f553eee56f5f6193c9432047c8c301dc5
Attila Mathe <attila.mathe@dnet-ebusiness-suite.com>
2013-10-31 07:45:00 +0200

    Country Iso2 code in region

--- src/main/resources/en/nan21.dnet.module.bd.ui.extjs/ds/Region_Ds.js
+++ src/main/resources/en/nan21.dnet.module.bd.ui.extjs/ds/Region_Ds.js
@@ -1,5 +1,6 @@
 Ext.define(Dnet.ns.bd + "Region_Ds$Trl", {
 	countryId__lbl: "Country(ID)",
+	countryIso2__lbl: "Country(ISO-2)",
 	country__lbl: "Country",
 	iso__lbl: "ISO"
 });
--- src/main/resources/ro/nan21.dnet.module.bd.ui.extjs/ds/Region_Ds.js
+++ src/main/resources/ro/nan21.dnet.module.bd.ui.extjs/ds/Region_Ds.js
@@ -1,5 +1,6 @@
 Ext.define(Dnet.ns.bd + "Region_Ds$Trl", {
-	countryId__lbl: "ID ţara",
-	country__lbl: "Cod ţara",
+	countryId__lbl: "Ţară(ID)",
+	countryIso2__lbl: "Tară(ISO-2)",
+	country__lbl: "Ţară",
 	iso__lbl: "ISO"
 });
